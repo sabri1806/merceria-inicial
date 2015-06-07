8df6692c9b3705a49290fe8c6a6d42f490b9d18f
sabrina lucero <sabrinita185@gmail.com>
2015-06-06 23:46:37 -0300

    agrega dependencias de junit al proyecto

--- src/test/java/com/merceria/AdminArticuloTest.java
+++ src/test/java/com/merceria/AdminArticuloTest.java
@@ -1,10 +1,20 @@
 package com.merceria;
 
+import org.junit.Test;
+
 public class AdminArticuloTest {
 	
 	//Aca hace el tes de consulta
+	@Test
+	public void obtenerArt_articuloPorId_retornaArt(){
+		
+	}
 	
 	
 	//aca hace el test de borrado
+	@Test
+	public void borrarArticulo_articuloConIdExistente_retornaIdArticuloBorrado(){
+	
 
+    }
 }
