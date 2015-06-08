d5d5d0a7915eea4c9bb44b622326ac52ce85f385
sabrina lucero <sabrinita185@gmail.com>
2015-06-07 23:04:14 -0300

    agrego preparacion, ejecucion, verificacion al test, crea clases de model y service

---
+++ src/main/java/com/service/ArticuloService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import com.merceria.model.Articulo;
+
+public class ArticuloService {
+
+	public Articulo obtenerArt(long id) {
+		// TODO Auto-generated method stub
+		Articulo art = null;
+		return art;
+	}
+
+	
+
+}
--- src/test/java/com/merceria/AdminArticuloTest.java
+++ src/test/java/com/merceria/AdminArticuloTest.java
@@ -1,20 +1,29 @@
 package com.merceria;
 
+import static org.junit.Assert.*;
+
+
 import org.junit.Test;
 
-public class AdminArticuloTest {
-	
-	//Aca hace el tes de consulta
-	@Test
-	public void obtenerArt_articuloPorId_retornaArt(){
-		
-	}
-	
-	
-	//aca hace el test de borrado
-	@Test
-	public void borrarArticulo_articuloConIdExistente_retornaIdArticuloBorrado(){
-	
+import com.merceria.model.Articulo;
+import com.service.ArticuloService;
 
-    }
+public class AdminArticuloTest {
+
+	// Aca hace el tes de consulta
+	@Test
+	public void obtenerArt_articuloPorId_retornaArt() {
+		ArticuloService articuloService = new ArticuloService();
+
+		long id = 1;
+		Articulo articulo = articuloService.obtenerArt(id);
+
+		assertEquals(1L,articulo.getId());
+	}
+
+	// aca hace el test de borrado
+	@Test
+	public void borrarArticulo_articuloConIdExistente_retornaIdArticuloBorrado() {
+
+	}
 }
